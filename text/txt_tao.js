e0452f56eed56ec6b627ffd1a8498b8bdf602787
Bryce Weiner <bryce@altsystem.io>
2016-09-22 23:18:09 -0700

    Rebranding

--- text/txt_tao.js
+++ text/txt_tao.js
@@ -4,54 +4,54 @@ module.exports = {
     // HELPTEXT
         'helpText': [
     //'title':
-            '*DashBot commands* \n',
+            '*TaoBot commands* \n',
 
     //'help_balance':
             '*balance*\t\task the bot for your current balance\n' +
-    '\t\t_@DashBot what is my balance_',
+    '\t\t_@TaoBot what is my balance_',
 
     //'help_send':
             '*send*\t\t\t\ttell the bot to send coins to someone; \n' +
-    '\t\t_@DashBot send 0.1 TAO to @someone_ \n' +
+    '\t\t_@TaoBot send 0.1 TAO to @someone_ \n' +
     '\t\t_aliases: give, tip_ \n' +
     '\t\t\t\tWorks also with major fiat currencies (use *currencies* command to see the list); \n' +
-    '\t\t_@DashBot give 4 USD to @someone_',
+    '\t\t_@TaoBot give 4 USD to @someone_',
 
     // 'help_deposit':
-            '*deposit*\t\task the bot for a deposit address; _@DashBot let me deposit!_',
+            '*deposit*\t\task the bot for a deposit address; _@TaoBot let me deposit!_',
 
     // 'help_withdraw':
             '*withdraw*\ttell the bot to withdraw an amount to an address. \n' +
     '\t\t\t\t\tYou can also use _all_ to withdraw your complete balance (minus the tx fee) \n' +
-    '\t\t_@DashBot withdraw 1 TAO to Xdice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp!_',
+    '\t\t_@TaoBot withdraw 1 TAO to Xdice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp!_',
 
     // 'help_currencies':
             '*currencies*\task the bot for a list of supported currencies.\n' +
-    '\t\t_@DashBot what currencies do you know?_ ',
+    '\t\t_@TaoBot what currencies do you know?_ ',
 
     // 'help_price':
             '*price*\t\t\task the bot for the Tao price in a particular currency. Price info from coinmarketcap.\n ' +
-    '\t\t_@DashBot price in USD!_ ',
+    '\t\t_@TaoBot price in USD!_ ',
 
     // 'help_pricelist':
             '*priceticker*\t\tshows all the known prices. Use * priceticker short* for a limited list.',
 
     // 'help_convert':
             '*convert*\t\task the bot to convert between a particular currency and Tao (or visa versa).\n' +
-    '\t\t_@DashBot 0.03 TAO to GBP_ \t or \t _@DashBot 15 EURO to DASH_\n',
+    '\t\t_@TaoBot 0.03 TAO to GBP_ \t or \t _@TaoBot 15 EURO to DASH_\n',
 
     // 'help_rain':
     // '*rain*\t\tcheck the available rain and threshold. \n ' +
     // '\t\tAdmins can also release the rain with the *rain now* command.\n',
 
     // 'help_sun':
-            '*sun*\t\t\tcheck the available sunshine. \n' +
-    '\t\t\tEach user that has tipped another user will receive a _sunray_ (read: free Tao) from the sun fund.',
+            //'*sun*\t\t\tcheck the available sunshine. \n' +
+    //'\t\t\tEach user that has tipped another user will receive a _sunray_ (read: free Tao) from the sun fund.',
 
     // 'help_quiz'
-            '*quiz add*\t\tThe bot will ask to input a new quiz question and answer.\nEach question needs to be reviewed by a moderator.\n',
+    //        '*quiz add*\t\tThe bot will ask to input a new quiz question and answer.\nEach question needs to be reviewed by a moderator.\n',
     // + '\n\t\tUse _quiz list_ and _quiz review_ to get the question number for the next commands.\n' +
-            '*quiz answer*\t\tPost an answer to a quiz question.\n'
+    //        '*quiz answer*\t\tPost an answer to a quiz question.\n'
         ],
 
         'helpAdminOnly':
@@ -61,30 +61,30 @@ module.exports = {
     '*balance all*\tshow all the tip jars (must be enabled in code)\n' +
     '\n' +
     '*balance check*\tshow the balance of a specific user (must be enabled in code) \n' +
-    '\t\t_@dashbot balance check @naruby_ \n' +
+    '\t\t_@TaoBot balance check @naruby_ \n' +
     '\n\n' +
-    '*whisper*\tSend a message in a private channel to a user as dashbot.\n' +
+    '*whisper*\tSend a message in a private channel to a user as TaoBot.\n' +
     '\t\t\t\t\tUse case :moderator warning.\n' +
-    '\t\t_@Dashbot whisper @narbuy stop being silly man._\n' +
+    '\t\t_@TaoBot whisper @narbuy stop being silly man._\n' +
     '\n\n' +
-    '*sun threshold*\t set the threshold on where the balance of the sun account will be distributed\n' +
-    '\t\t\t\tbetween all the users that tipped. Defaults to 5 Tao.\n' +
-    '\t\t_@dashbot sun threshold 2.5 dash_\n' +
-    '\n' +
-    '*sun eligible*\tSee which users are eligible for a sunray.\n' +
-    '\n' +
-    '*sun reset*\t\tReset all tip counts, not needed normally as tip counters are reset when sun is shining.' +
-    '\n' +
-    '*quiz list*\t\tShow all approved questions.\n' +
-    '*quiz review*\tList all questions that need to be reviewed (reward = 0).\n' +
-    '*quiz delete _question number_*\tDelete a question.\n' +
-    '*quiz reward _question number_*\tSet/change reward for a question (also approves the question). @dashbot will ask amount.\n' +
-    '*quiz start*\t\tStart a quiz.\n' +
-    '*quiz abort*\t\tStop a quiz without showing results.\n'+
-    '*quiz next*\t\tSkip the current question (if no one finds the answer).\n',
+    //'*sun threshold*\t set the threshold on where the balance of the sun account will be distributed\n' +
+    //'\t\t\t\tbetween all the users that tipped. Defaults to 5 Tao.\n' +
+    //'\t\t_@TaoBot sun threshold 2.5 dash_\n' +
+    //'\n' +
+    //'*sun eligible*\tSee which users are eligible for a sunray.\n' +
+    //'\n' +
+    //'*sun reset*\t\tReset all tip counts, not needed normally as tip counters are reset when sun is shining.' +
+    //'\n' +
+    //'*quiz list*\t\tShow all approved questions.\n' +
+    //'*quiz review*\tList all questions that need to be reviewed (reward = 0).\n' +
+    //'*quiz delete _question number_*\tDelete a question.\n' +
+    //'*quiz reward _question number_*\tSet/change reward for a question (also approves the question). @TaoBot will ask amount.\n' +
+    //'*quiz start*\t\tStart a quiz.\n' +
+    //'*quiz abort*\t\tStop a quiz without showing results.\n'+
+    //'*quiz next*\t\tSkip the current question (if no one finds the answer).\n',
 
         'tx_fee': 'The transaction fee is set to ',
-        'HelpRandom1': 'Here is an example of one of my commands, type "@dashbot help" for my full list. ',
+        'HelpRandom1': 'Here is an example of one of my commands, type "@TaoBot help" for my full list. ',
     // 'HelpRandom2': 'You can get information about all my other cool tricks via the *help* command. \n Have a nice day !',
     // NEW USER
         'WarningNewUser1': 'The new user ',
